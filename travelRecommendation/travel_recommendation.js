9388ec90ec608d29fc2be378fd470a419114d50d
Herve MARTINOT <h.martinot13@gmail.com>
2026-04-26 19:16:30 -0400

    Javascript Module5 Hand-on2-8

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -1,29 +1,62 @@
 const btnSearch = document.getElementById("btnSearch");
 const MyReset = document.getElementById("btnReset"); 
+let i=0;
 
 function resetSearch() {
   document.getElementById("btnSearch").value = "";
+  resultDiv11.innerHTML = '';
+  resultDiv12.innerHTML = '';
+  resultDiv21.innerHTML = '';
+  resultDiv22.innerHTML = '';
+  resultDiv31.innerHTML = '';
+  resultDiv32.innerHTML = '';
 }
 
 function searchCondition() {
     const input = document.getElementById('conditionInput').value.toLowerCase();
-	const result = document.getElementById('resultDiv');
-	result.innerHTML = '';
+	//const result1 = document.getElementById('resultDiv1');
+	//const result2 = document.getElementById('resultDiv2');
+    //const result3 = document.getElementById('resultDiv3');
+    resultDiv11.innerHTML = '';
+    resultDiv12.innerHTML = '';
+    resultDiv21.innerHTML = '';
+    resultDiv22.innerHTML = '';
+    resultDiv31.innerHTML = '';
+    resultDiv32.innerHTML = '';
 
 	fetch('travel_recommendation_api.json')
-	.then(response => response.json())
-	.then(data => {
-	const theCondition = data.countries.find(item => item.name.toLowerCase() === input);
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Erreur lors du chargement du fichier JSON');
+        }
+        //i=0;
+        return response.json();
+      })
+      .then(data => {
+        data.countries.forEach(country => {
+          if (country.name.toLowerCase() === input) {
+                country.cities.forEach(city => {
+                if (i===0){
+                        resultDiv11.innerHTML += `<img src="${city.imageUrl}" alt="hjh">`;
+                        resultDiv21.innerHTML += `<h2>${city.name}</h2>`;
+                        resultDiv31.innerHTML += `<p><strong>Description:</strong>${city.description}</p>`;
+                        i += 1; 
+                    } else {
+                        resultDiv12.innerHTML += `<img src="${city.imageUrl}" alt="hjh">`;
+                        resultDiv22.innerHTML += `<h2>${city.name}</h2>`;
+                        resultDiv32.innerHTML += `<p><strong>Description:</strong>${city.description}</p>`;
+                        i = 0; 
+                }
+                });
+            }
+      })
 
-	if (theCondition) {
-			  const cities = theCondition.cities.join(', ');
-			  resultDiv.innerHTML += `<img src="${cities.imageUrl}" alt="hjh">`;
-			  resultDiv.innerHTML += `<h2>${cities.name}</h2>`;
-			  resultDiv.innerHTML += `<p><strong>Description:</strong> ${cities.description}</p>`;
-	} else {
-			  resultDiv.innerHTML = 'cities not found.';
-	}
-
+      .catch(error => {
+        console.error('Erreur:', error);
+      });
+    })
+}
+            
 /*	if (theCondition === temples) {
 			  const temples = theCondition.temples.join(', ');
 			  resultDiv.innerHTML += `<img src="${temples.imageUrl}" alt="hjh">`;
@@ -39,14 +72,14 @@ function searchCondition() {
 			  resultDiv.innerHTML += `<h2>${beaches.name}</h2>`;
 			  resultDiv.innerHTML += `<p><strong>Description:</strong> ${beaches.description}</p>`;
 	} else {
-			  resultDiv.innerHTML = 'beaches not found.';
-	} */
+			resultDiv.innerHTML = 'beaches not found.';
+	} 
 
 	})
 	.catch(error => {
 			console.error('Error:', error);
 			resultDiv.innerHTML = 'An error occurred while fetching data.';
-	});
-}
+	});*/
+
 btnSearch.addEventListener('click', searchCondition);
 
